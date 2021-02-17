ded7a3e44a19ec7bc862e1a1306ed1ad3b99f585
sanatg <62789975+sanatg@users.noreply.github.com>
2021-02-17 21:51:31 +0530

    Update DatabaseSdk.js

--- DatabaseSdk.js
+++ DatabaseSdk.js
@@ -32,3 +32,31 @@
        
     })
   } 
+function facebooksignin(){
+const FacebookAuth =  new firebase.auth.FacebookAuthProvider(); 
+ firebase.auth().signInWithPopup(FacebookAuth)
+ .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var accessToken = credential.accessToken;
+
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+
+      // ...
+    });
+  // [END auth_facebook_signin_popup]
+}
